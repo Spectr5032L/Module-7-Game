3ac1e5fd7204ad660e84344feb7fe0124c7bd4c7
Spectr5032L <101048182+Spectr5032L@users.noreply.github.com>
2023-06-12 15:17:09 +0500

    Game + Bug Fix

--- Module 7/Assets/Script/GlobalConstants.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobalConstants : MonoBehaviour
-{
-    public static bool gameHasStarted = false;
-    public static int SENSITIVITY = 1000;
-}
